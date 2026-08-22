07546467c7f1a94fd9388403888baf9cb78c6f4c
gustavo_sena <gustavo_sena-alves@estudante.sesisenai.org.br>
2026-08-22 09:35:48 -0300

    ADD -  começando a fazer atividade

---
+++ Cadastro_alunos/pages/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        Nome:<input type="text" name="nome">
+        Email:<input type="text" name="nome">
+        Idade:<input type="text" name="nome">
+        Selcione o seu curso: <select name="curso">
+            <!--<select name="curso">
+                <option value="valor1">Desenvolvimento de sistema</option>
+                <option value="valor2" selected>Steam</option>
+                <option value="valor3">Química</option>
+            </select>-->
+            <!-- O segundo valor estará selecionado inicialmente -->
+<select name="curso">
+    <option value="">Selecione seu curso</option>
+    <option value="ds">Desenvolvimento de Sistemas</option>
+    <option value="adm">Administração</option>
+    <option value="eletronica">Eletrônica</option>
+</select>
+
+    </form>
+</body>
+</html>
