72520b35c8cbcf0e4c568ab535934d296deb7a04
gustavo_sena <gustavo_sena-alves@estudante.sesisenai.org.br>
2026-08-22 12:14:25 -0300

    UP - coloquei os comandos de envio php e fiz o formulario

--- Cadastro_alunos/pages/index.php
+++ Cadastro_alunos/pages/index.php
@@ -6,24 +6,69 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
-        Nome:<input type="text" name="nome">
-        Email:<input type="text" name="nome">
-        Idade:<input type="text" name="nome">
-        Selcione o seu curso: <select name="curso">
-            <!--<select name="curso">
-                <option value="valor1">Desenvolvimento de sistema</option>
-                <option value="valor2" selected>Steam</option>
-                <option value="valor3">Química</option>
-            </select>-->
-            <!-- O segundo valor estará selecionado inicialmente -->
-<select name="curso">
-    <option value="">Selecione seu curso</option>
-    <option value="ds">Desenvolvimento de Sistemas</option>
-    <option value="adm">Administração</option>
-    <option value="eletronica">Eletrônica</option>
-</select>
 
+    <form action="formAluno" method="POST">
+        Nome: <input type="text" name="nome"><br>
+        Email: <input type="text" name="email"><br>
+        Idade: <input type="text" name="idade"><br>
+
+        Selecione o seu curso:
+        <select name="curso">
+            <option value="">Selecione seu curso</option>
+            <option value="ds">Desenvolvimento de Sistemas</option>
+            <option value="adm">Administração</option>
+            <option value="eletronica">Eletrônica</option>
+        </select>
+
+        <input type="submit" value="Enviar">
     </form>
+    <div>
+
+            <table>
+                <thead>
+                    <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Idade</th>
+                    <th scope="col">Curso</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">Chris</th>
+                    <td>HTML tables</td>
+                    <td>22</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Dennis</th>
+                    <td>Web accessibility</td>
+                    <td>45</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Sarah</th>
+                    <td>JavaScript frameworks</td>
+                    <td>29</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Karen</th>
+                    <td>Web performance</td>
+                    <td>36</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                    <th scope="row" colspan="2">Average age</th>
+                    <td>33</td>
+                    </tr>
+                </tfoot>
+                </table>
+    </div>
+
+    <style>
+        table, tr, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 </body>
 </html>
